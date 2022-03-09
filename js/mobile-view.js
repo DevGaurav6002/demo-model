84685417b7b12e1552093f5c6bf972187e401ea8
gaurav <g.ujagare7@gmail.com>
2022-03-09 11:52:46 +0530

    slider has been fixed

--- js/mobile-view.js
+++ js/mobile-view.js
@@ -3,10 +3,10 @@ const slides = Array.from(document.querySelectorAll('.main-image-container .img'
 
 let isDragging = false;
 let startPos = 0;
-let currentTranslate = 0;
-let prevTranslate = 0;
-let animationId = 0;
 let currentIndex = 0;
+let endPos = 0;
+let count = 0;
+
 
 slides.forEach((slide, index) => {
     const slideImage = slide.querySelector('img')
@@ -14,73 +14,47 @@ slides.forEach((slide, index) => {
         e.preventDefault();
     })
 
-    //touch Events
-
     slide.addEventListener('touchstart', touchStart(index))
-    slide.addEventListener('touchend', touchEnd)
-    slide.addEventListener('touchmove', touchMove)
+    slide.addEventListener('touchend', (e) => {
+        endPos = e.changedTouches[0].clientX;
+
+        if (startPos > endPos) {
+            count++
+            couracel()
+        }
+        else {
+            count--
+            couracel()
+        }
+        console.log("start" + startPos + "end" + endPos)
+    })
 })
 
-//disable context menu
-// window.oncontextmenu = function (e) {
-//     e.preventDefault()
-//     e.stopPropagation()
-//     return false
-// }
+
 
 function touchStart(index) {
     return function (event) {
-        currentIndex = index
         startPos = getPositionX(event)
         console.log(startPos)
-        isDragging = true;
-
-        animationId = requestAnimationFrame(animation)
-
-        console.log("start")
     }
+}
+
+
+function couracel() {
+
+    if (count === slides.length) {
+        count = 0;
+    }
+
+    if (count < 0) {
+        count = slides.length - 1
+    }
+
+    slides.forEach(slide => {
+        slide.style.transform = `translate(-${count * 100 - 10}%)`
+    })
 }
 
 function getPositionX(event) {
     return event.touches[0].clientX
-}
-
-function touchEnd() {
-    isDragging = false;
-    cancelAnimationFrame(animationId)
-
-    const movedBy = currentTranslate - prevTranslate
-
-    if (movedBy < -20 && currentIndex < slides.length - 1) {
-        currentIndex += 1
-    }
-
-    if (movedBy > 20 && currentIndex > 0) {
-        currentIndex -= 1
-    }
-
-}
-
-function touchMove() {
-    if (isDragging) {
-        let currentPosistion = getPositionX(event)
-        currentTranslate = prevTranslate + currentPosistion - startPos
-    }
-}
-
-function animation() {
-    setSliderPosition()
-    if (isDragging) {
-        requestAnimationFrame(animation)
-    }
-}
-
-function setSliderPosition() {
-    return slider.style.transform = `translateX(${currentTranslate}px)`
-}
-
-function setPositionByIndex() {
-    currentTranslate = currentIndex * -window.innerWidth
-    prevTranslate = currentTranslate
-    setSliderPosition()
 }
